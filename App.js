b650d8abd414286b7e4fd2cbf1f2d7ec2735bd58
Ben Weber <benweberswe@gmail.com>
2023-04-11 12:30:04 -0700

    adding location data

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 /*
     Pull weather data
 */
-
 async function getWeatherData(){
     try {
         const weatherData = await fetch('https://api.weather.gov/', {headers :{
@@ -14,7 +13,20 @@ async function getWeatherData(){
     }
 }
 
+function getLocationData() {
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(function(position) {
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
+            console.log(latitude)
+            console.log(longitude)
+        })
+    } else {
+        console.log('no geolocation data found')
+    }
+}
+
 const button = document.getElementById('button');
 button.addEventListener('click', function() {
-    getWeatherData();
+    getLocationData();
 })
