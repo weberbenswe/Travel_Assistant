f3a9811104d75decca42d7e6abcff1f7b6641158
Ben Weber <benweberswe@gmail.com>
2023-04-11 12:05:14 -0700

    connecting through async

--- App.js
+++ App.js
@@ -3,3 +3,15 @@
 */
 
 const userAgent = '{User-Agent: (travelAssistantbw.com, benweberswe@gmail.com)}';
+
+async function getWeatherData(){
+    try {
+        const weatherData = await fetch('https://api.weather.gov/');
+        console.log(weatherData);
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+getWeatherData();
