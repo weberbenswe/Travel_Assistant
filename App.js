22d78f4978db97fde6b7da4c7e4705b18bb4f5f8
Ben Weber <benweberswe@gmail.com>
2023-04-11 12:13:39 -0700

    updating to add user agent

--- App.js
+++ App.js
@@ -6,8 +6,11 @@ const userAgent = '{User-Agent: (travelAssistantbw.com, benweberswe@gmail.com)}'
 
 async function getWeatherData(){
     try {
-        const weatherData = await fetch('https://api.weather.gov/');
+        const weatherData = await fetch('https://api.weather.gov/', {headers :{
+            'User-Agent': '(myweatherapp.com, contact@myweatherapp.com)'
+    }});
         console.log(weatherData);
+        console.log(weatherData.headers)
     }
     catch(error){
         console.log(error);
