834135fcd3d21d7b529ad18dc00341988617e46f
Ben Weber <benweberswe@gmail.com>
2023-04-11 12:25:22 -0700

    adding frontend driver

--- App.js
+++ App.js
@@ -2,19 +2,19 @@
     Pull weather data
 */
 
-const userAgent = '{User-Agent: (travelAssistantbw.com, benweberswe@gmail.com)}';
-
 async function getWeatherData(){
     try {
         const weatherData = await fetch('https://api.weather.gov/', {headers :{
             'User-Agent': '(myweatherapp.com, contact@myweatherapp.com)'
     }});
         console.log(weatherData);
-        console.log(weatherData.headers)
     }
     catch(error){
         console.log(error);
     }
 }
 
-getWeatherData();
+const button = document.getElementById('button');
+button.addEventListener('click', function() {
+    getWeatherData();
+})
