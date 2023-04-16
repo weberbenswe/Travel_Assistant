1af3120d188edea3b17c5ea597c184151274eab2
Ben Weber <benweberswe@gmail.com>
2023-04-16 09:22:01 -0700

    outline of process

---
+++ ml.js
@@ -0,0 +1,9 @@
+/**
+ * Gather Data: Use the weather.gov API to collect weather data. The National Weather Service (NWS) API allows developers access to critical forecasts, alerts, and observations, along with other weather data1.
+    Prepare Data: Clean and preprocess the data to make it suitable for use in your machine learning model.
+    Choose a Model: Select an appropriate machine learning algorithm for your problem. For example, you could use a convolutional neural network to develop a machine learning weather prediction system2.
+    Train: Train your model on the prepared weather data.
+    Evaluate: Evaluate the performance of your model.
+    Hyperparameter Tuning: Fine-tune the hyperparameters of your model to improve its performance.
+    Predict: Use your trained model to make predictions about future weather conditions. 
+*/
